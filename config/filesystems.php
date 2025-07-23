a495ffd53c0550c17c0327cb512f5592adf84596
Maaz Khalid <imaazkhalid@gmail.com>
2025-07-23 21:07:08 +0500

    fix s3 setup

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,7 +39,7 @@ return [
         ],
 
         'public' => [
-            'driver' => 'local',
+            'driver' => 's3',
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
@@ -58,6 +58,7 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
             'report' => false,
+            'visibility' => 'public'
         ],
 
     ],
